248e395230870518f8a9d061251f39c710d8dc54
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-19 00:16:08 +0530

    add & store expense into firebase

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,309 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter/rendering.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  // var _enteredTitle = '';
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // } => Can use this feature but need to define for each which could be performance loss
+
+  final _db = FirebaseFirestore.instance; //import the root database
+
+  // createExpense(Expense user) async{
+  //   _db.collection("expenses").add(expense.)
+  // }
+
+  final _titleController = TextEditingController();
+  final _priceController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month - 1, now.day);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  Future<void> uploadExpense() async {
+    try {
+      final data = await FirebaseFirestore.instance.collection("expenses").add({
+        "Title": _titleController.text.trim(),
+        "Amount": _priceController.text.trim(),
+        "Date": _selectedDate,
+        "SavedAt": FieldValue.serverTimestamp(),
+        "Category": _selectedCategory.name,
+      });
+      Navigator.pop(context);
+      //print(data.id);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  //can use then now.then coz date would be coming from future and hence this way it would be allowed
+  //more convenient way is to use async and await coz Date is a future value
+
+  void _submitExpenseData() {
+    final enteredAmount =
+        double.tryParse(//tryParse won't through error instead would return null
+            _priceController.text); //String '1.12' would be treated as double
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty || amountIsInvalid) {
+      showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Invalid Input'),
+                content:
+                    const Text('Please make sure a valid input is Entered!'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(ctx);
+                      },
+                      child: const Text('Okay'))
+                ],
+              ));
+      //Show Error Message
+      return;
+    }
+
+    widget.onAddExpense(Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory));
+    Navigator.pop(
+        context); //after a successful submission automatic closing done
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _priceController.dispose();
+    super.dispose();
+
+    //controller could be simpler choice but dropdown doesn't support that
+  }
+
+  //Using controller flutter automatically handles everything and allows for simpler use
+  //also while using controller it is necessary to dispose it when it's not in use
+  //or at the end of the widget to allow for memory saving
+
+  @override
+  Widget build(BuildContext context) {
+    final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
+    //New Widget works like media query but now we are able to use dimensions anywhere
+
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        final width = constraints.maxWidth;
+        return SizedBox(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  16,
+                  48,
+                  5,
+                  keyboardSpace +
+                      16), //top was blocking maybe in some devices more, so set it to greater value would work for any device
+              child: Column(
+                children: [
+                  if (width >= 600)
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _titleController,
+                            maxLength: 50,
+                            decoration: const InputDecoration(
+                              label: Text('Title'),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 24,
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: _priceController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              prefixText: '₹  ',
+                              label: Text('Amount'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  else
+                    TextField(
+                      //can use max length and keyboard type too, useful features.
+                      //onChanged: _saveTitleInput,
+                      controller: _titleController,
+                      maxLength: 50,
+                      decoration: const InputDecoration(
+                        label: Text('Title'),
+                      ),
+                    ),
+                  if (width >= 600)
+                    Row(
+                      children: [
+                        DropdownButton(
+                            value: _selectedCategory,
+                            items: Category.values
+                                .map(
+                                  (category) => DropdownMenuItem(
+                                    value:
+                                        category, //showing selected category rather than blank
+                                    child: Text(
+                                      category.name.toUpperCase(),
+                                    ),
+                                  ),
+                                )
+                                .toList(), //It was an iterable type error so need to explicitly use the toList method to make it
+                            onChanged: (value) {
+                              if (value == null) {
+                                return;
+                              }
+                              setState(() {
+                                _selectedCategory = value;
+                              });
+                            }),
+                        const SizedBox(
+                          width: 24,
+                        ),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(_selectedDate == null
+                                  ? 'No Date Selected'
+                                  : formatter.format(_selectedDate!)),
+                              //Logic for showing selected date when chosen any
+                              IconButton(
+                                  onPressed: _presentDatePicker,
+                                  icon: const Icon(Icons.calendar_month)),
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  else
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _priceController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              prefixText: '₹  ',
+                              label: Text('Amount'),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(_selectedDate == null
+                                  ? 'No Date Selected'
+                                  : formatter.format(_selectedDate!)),
+                              //Logic for showing selected date when chosen any
+                              IconButton(
+                                  onPressed: _presentDatePicker,
+                                  icon: const Icon(Icons.calendar_month)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  if (width >= 600)
+                    Row(
+                      children: [
+                        const Spacer(),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel')),
+                        ElevatedButton(
+                            onPressed: _submitExpenseData,
+                            child: const Text('Save Expense'))
+                      ],
+                    )
+                  else
+                    Row(
+                      children: [
+                        DropdownButton(
+                            value: _selectedCategory,
+                            items: Category.values
+                                .map(
+                                  (category) => DropdownMenuItem(
+                                    value:
+                                        category, //showing selected category rather than blank
+                                    child: Text(
+                                      category.name.toUpperCase(),
+                                    ),
+                                  ),
+                                )
+                                .toList(), //It was an iterable type error so need to explicitly use the toList method to make it
+                            onChanged: (value) {
+                              if (value == null) {
+                                return;
+                              }
+                              setState(() {
+                                _selectedCategory = value;
+                              });
+                            }),
+                        const Spacer(),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel')),
+                        ElevatedButton(
+                            onPressed: () async {
+                              uploadExpense();
+                            },
+                            child: const Text('Save Expense'))
+                      ],
+                    )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    //without single child we weren't able to reach other text fields
+  }
+}
