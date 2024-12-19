8cf0aec8f66441bf7dfb1ddcf6476e6c48d24b1b
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-19 22:09:23 +0530

    Firebase Auth Setup

---
+++ lib/screens/login.dart
@@ -0,0 +1,260 @@
+import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'signup.dart';
+import 'reset.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String email = "", password = "";
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  String errorMessage = '';
+  final _formkey = GlobalKey<FormState>();
+
+  Future<void> signIn() async {
+    try {
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailcontroller.text.trim(),
+        password: passwordcontroller.text.trim(),
+      );
+
+      if (userCredential.user != null) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Login Successful"),
+              content: const Text("You have successfully logged in."),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Expenses()),
+                    );
+                  },
+                  child: const Text("OK"),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      String error = '';
+
+      if (e.code == 'user-not-found') {
+        error = "No user found for that email.";
+      } else if (e.code == 'wrong-password') {
+        error = "Wrong password provided.";
+      } else if (e.code == 'invalid-email') {
+        error = "Invalid email format.";
+      } else {
+        error = "Check your credentials.";
+      }
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Login Failed"),
+            content: Text(error),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("OK"),
+              ),
+            ],
+          );
+        },
+      );
+
+      setState(() {
+        errorMessage = error;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          margin: const EdgeInsets.all(24),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _header(context),
+                _inputField(context),
+                _forgotPassword(context),
+                if (errorMessage.isNotEmpty)
+                  Text(
+                    errorMessage,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                _signup(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _header(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/brand/logo.png",
+            height: 75,
+            width: 250,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            "Login Page",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            "Enter your credentials to login",
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _inputField(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextFormField(
+          controller: emailcontroller,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please Enter E-Mail';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            hintText: "Email",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
+            fillColor:
+                const Color.fromARGB(255, 109, 113, 147).withOpacity(0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.person),
+          ),
+        ),
+        const SizedBox(height: 20),
+        TextFormField(
+          controller: passwordcontroller,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please Enter Password';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
+            fillColor:
+                const Color.fromARGB(255, 109, 113, 147).withOpacity(0.1),
+            filled: true,
+            prefixIcon: const Icon(Icons.lock),
+          ),
+          obscureText: true,
+        ),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {
+            if (_formkey.currentState!.validate()) {
+              setState(() {
+                email = emailcontroller.text;
+                password = passwordcontroller.text;
+              });
+            }
+            signIn();
+          },
+          style: ElevatedButton.styleFrom(
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor: Colors.black,
+          ),
+          child: const Text(
+            "Login",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+
+  _forgotPassword(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ResetPasswordPage()));
+      },
+      child: const Text(
+        "Forgot password?",
+        style:
+            TextStyle(color: Color.fromARGB(255, 21, 102, 153), fontSize: 18),
+      ),
+    );
+  }
+
+  _signup(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Don't have an account? ",
+          style: TextStyle(fontSize: 18, color: Colors.black),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SignupPage()),
+            );
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(
+                color: Color.fromARGB(255, 21, 102, 153), fontSize: 18),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    emailcontroller.dispose();
+    passwordcontroller.dispose();
+    super.dispose();
+  }
+}
