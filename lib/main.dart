6dab8e8af15ecddb23f075179e51612561284b38
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-19 22:10:34 +0530

    Login screen part corrected

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
-import 'package:expense_tracker/widgets/screens/login.dart';
+import 'package:expense_tracker/screens/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
